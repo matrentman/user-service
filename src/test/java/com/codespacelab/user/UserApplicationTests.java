4deafac33dce53baa385b4d050c33d3b84aca468
Mike Trentman <mikeatrentman@yahoo.com>
2021-07-21 18:11:48 -0400

    init

---
+++ src/test/java/com/codespacelab/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codespacelab.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
